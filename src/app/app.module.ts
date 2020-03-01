94787ccd97d7dc561c2e41cd195f53df05bef558
Manol Trendafilov <manol.trendafilov@gmail.com>
2020-03-01 22:32:58 +0200

    initialize app, add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {appRouting} from './app.routing';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    appRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
