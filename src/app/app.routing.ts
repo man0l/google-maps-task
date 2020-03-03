f68c0fb9fdf97001f2ce2487cee813d44d632540
Manol Trendafilov <manol.trendafilov@gmail.com>
2020-03-04 00:00:22 +0200

    remomve unused components

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,9 @@
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {MapComponent} from './map/map.component';
 import {ModuleWithProviders} from '@angular/core';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'map', component: MapComponent },
     { path: '**', component: HomeComponent }
 ];
 
