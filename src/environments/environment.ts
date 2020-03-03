017a3c73d04328d4a60089fb090b28799bb6da30
Manol Trendafilov <manol.trendafilov@gmail.com>
2020-03-03 20:20:10 +0200

    change the base url tolocalhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://127.0.0.1:8000',
+  baseUrl: 'http://localhost:8000',
   endpoint: {
     google: '/maps/google',
     openStreet: '/maps/openstreet'
