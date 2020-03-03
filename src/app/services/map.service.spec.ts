ab8f34bbda58cb451bc5b91e0fe4ba0bea832f57
Manol Trendafilov <manol.trendafilov@gmail.com>
2020-03-04 00:04:56 +0200

    add unit test for get coordinates

--- src/app/services/map.service.spec.ts
+++ src/app/services/map.service.spec.ts
@@ -15,4 +15,15 @@ describe('MapService', () => {
     expect(service).toBeTruthy();
   }));
 
+  it('should call and return google maps coordinates', inject([MapService], (service: MapService) => {
+    const data = of({
+      address: '1 Hacker Way, Menlo Park, CA 94025, USA',
+      latitude: '37.485073',
+      longitude: '-122.1482824'
+    });
+     const spyOSM    = spyOn(service, 'getCoordinatesOpenStreet').and.returnValue(data);
+     const spyGoogle = spyOn(service, 'getCoordinatesGoogle').and.returnValue(data);
+
+     expect(service.getCoordinates('any')).toBe([data, data]);
+  }));
 });
